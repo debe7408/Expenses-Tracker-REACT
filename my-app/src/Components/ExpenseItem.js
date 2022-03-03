3b3c6923b4661e85a72ab29267157479ce63f1ea
Deividas Bendaravičius <bendaravicius.deividas@gmail.com>
2022-03-03 19:10:45 +0200

    Extracting hardcoded data

--- my-app/src/Components/ExpenseItem.js
+++ my-app/src/Components/ExpenseItem.js
@@ -1,13 +1,21 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
   return (
     <div className="expense-item">
-      <div>March 20th 2019</div>
-      <div className="expense-item__description">
-        <h2>Car insurance</h2>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
       </div>
-      <div className="expense-item__price">259.56</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
   );
 }
